82b857d6eb8a49a76c74b859956d9baaa312d926
rrcoder0167 <riddhiman.rana@gmail.com>
2023-03-05 21:40:19 -0800

    Add commit message:Add user friends, server chat, and add friends button

--- static/orange_ui.js
+++ static/orange_ui.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+    //tab bar functionality
     const tabs = document.querySelectorAll('.tab');
     const slide = document.querySelector('.slide');
     const tabContents = document.querySelectorAll('.tab-content');
@@ -43,27 +44,70 @@ $(document).ready(() => {
     const firstTabContent = document.querySelector(`#${firstTabContentId}`);
     firstTabContent.style.display = 'block';
     
+    var dropdown = document.querySelector('.dropdown');
+    var submenu = dropdown.querySelector('.dropdown-submenu');
+    
+    dropdown.addEventListener('click', function(event) {
+      event.stopPropagation();
+      submenu.classList.toggle('show');
+    });
+    
+    document.addEventListener('click', function(event) {
+      if (!dropdown.contains(event.target)) {
+        submenu.classList.remove('show');
+      }
+    });
+
+    var profileBtn = $('.profile-btn');
+    var dropdownArrow = $('.dropdown-arrow');
+    var dropdownMenu = $('.dropdown-menu');
+    var isDropdownOpen = false;
+    dropdownArrow.css('display', 'none');//set the arrow to be invisible by default
+  
+    profileBtn.on('mouseenter', function() {
+      dropdownArrow.css('display', 'inline-block');
+    });
+  
+    profileBtn.on('mouseleave', function() {
+      if (!dropdownMenu.hasClass('open')) {
+        dropdownArrow.css('display', 'none');
+      }
+    });
+  
+    profileBtn.on('click', function(e) {
+      e.stopPropagation();
+      isDropdownOpen = !isDropdownOpen;
+      profileBtn.css('width', isDropdownOpen ? '250px' : '');
+      dropdownMenu.toggleClass('open');
+      dropdownArrow.toggleClass('open');
+      dropdownArrow.css('display', isDropdownOpen ? 'inline-block' : 'none');
+    });
+    
+    // profile btn js
+    $(document).on('click', function(e) {
+      if (!profileBtn.is(e.target) && !dropdownMenu.has(e.target).length) {
+        isDropdownOpen = false;
+        profileBtn.css('width', '');
+        dropdownMenu.removeClass('open');
+        dropdownArrow.removeClass('open');
+        dropdownArrow.css('display', 'none');
+      } else {
+        dropdownArrow.css('display', isDropdownOpen ? 'inline-block' : 'none');
+      }
+    });
+  
+    let userBadge = $('.user-badge');
+    userBadge.each(function() {
+      let description = $(this).find('.description');
+      let badgeRect = this.getBoundingClientRect();
+      let descriptionRect = description[0].getBoundingClientRect();
+  
+      if (descriptionRect.width > badgeRect.width) {
+        let halfDescriptionWidth = descriptionRect.width / 2;
+        let leftOffset = halfDescriptionWidth - (badgeRect.width / 2);
+  
+        description.css('left', `-${leftOffset}px`);
+      }
+    });
 });
 
-var dropdowns = document.querySelectorAll('.dropdown');
-
-dropdowns.forEach(function(dropdown) {
-  dropdown.addEventListener('click', function(event) {
-    event.stopPropagation();
-    var dropdownMenu = this.querySelector('.dropdown-submenu');
-    if (dropdownMenu.style.display === 'none') {
-      dropdownMenu.style.display = 'block';
-    } else {
-      dropdownMenu.style.display = 'none';
-    }
-  });
-});
-
-document.addEventListener('click', function(event) {
-  dropdowns.forEach(function(dropdown) {
-    var dropdownMenu = dropdown.querySelector('.dropdown-submenu');
-    if (dropdownMenu.style.display === 'block' && !dropdown.contains(event.target)) {
-      dropdownMenu.style.display = 'none';
-    }
-  });
-});
